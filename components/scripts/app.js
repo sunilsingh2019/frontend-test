2518843f9217a86be63ab565fff0d970bd877ee1
Supreet Purohit Prakash <supreet.official2020@gmail.com>
2022-09-25 12:11:41 +0545

    parallax

--- components/scripts/app.js
+++ components/scripts/app.js
@@ -14,7 +14,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
 //function called on window resize
 win.on('resize', function () {});
-win.on('load', function () {});
+win.on('load', function () {
+    parallaxImg()
+});
 
 
 /*****  Declare your functions here  ********/
@@ -170,4 +172,45 @@ function checkboxToggle() {
             }
         })
     })
+}
+
+function parallaxImg() {
+    $('.img-parallax').each(function () {
+        var img = $(this);
+        var imgParent = $(this).parent();
+
+        function parallaxImg() {
+            var speed = img.data('speed');
+            var imgY = imgParent.offset().top;
+            var winY = $(this).scrollTop();
+            var winH = $(this).height();
+            var parentH = imgParent.innerHeight();
+
+
+            // The next pixel to show on screen      
+            var winBottom = winY + winH;
+
+            // If block is shown on screen
+            if (winBottom > imgY && winY < imgY + parentH) {
+                // Number of pixels shown after block appear
+                var imgBottom = ((winBottom - imgY) * speed);
+                // Max number of pixels until block disappear
+                var imgTop = winH + parentH;
+                // Porcentage between start showing until disappearing
+                var imgPercent = ((imgBottom / imgTop) * 100) + (50 - (speed * 50));
+            }
+            img.css({
+                top: imgPercent + '%',
+                transform: 'translate(-50%, -' + imgPercent + '%)'
+            });
+        }
+        $(document).on({
+            scroll: function () {
+                parallaxImg();
+            },
+            ready: function () {
+                parallaxImg();
+            }
+        });
+    });
 }
